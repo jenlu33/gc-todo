c00ce62396838a56c22e0506e0318219f4a67e56
jenlu33 <nejul33@gmail.com>
2020-10-19 16:01:09 -0400

    Updates

---
+++ precache-manifest.2bf3fa350bc7194f878fc6ccbb2a17c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4380891f4e2e352bc4835404557ad37",
+    "url": "/jenlu33/gc-todo/index.html"
+  },
+  {
+    "revision": "4cf8bd9d2dfd1fbd23bd",
+    "url": "/jenlu33/gc-todo/static/css/2.e6ac38db.chunk.css"
+  },
+  {
+    "revision": "4cf8bd9d2dfd1fbd23bd",
+    "url": "/jenlu33/gc-todo/static/js/2.e022735d.chunk.js"
+  },
+  {
+    "revision": "f59da382ef0e40e6fd7e14f0c7baae9f",
+    "url": "/jenlu33/gc-todo/static/js/2.e022735d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a500a6dbfe59ecb9d27c",
+    "url": "/jenlu33/gc-todo/static/js/main.cdeb1e6a.chunk.js"
+  },
+  {
+    "revision": "24614297605116fc286a",
+    "url": "/jenlu33/gc-todo/static/js/runtime-main.b1fde519.js"
+  }
+]);
